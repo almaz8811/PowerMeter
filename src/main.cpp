bbbe4b8d5302029bed615966198013d9f5df1926
almaz8811 <almaz8811@gmail.com>
2019-07-06 05:02:44 +0300

    Настроил WiFiManager и WebUpdate

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,85 @@
-#include <Arduino.h>
-#include <FS.h>          //this needs to be first, or it all crashes and burns...
+#include <FS.h> //this needs to be first, or it all crashes and burns...
+
 #include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
+#define BLYNK_PRINT Serial
+
 #include <BlynkSimpleEsp8266.h>
+
 //needed for library
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+
 #include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
 
-#define BLYNK_PRINT Serial
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
 
 //define your default values here, if there are different values in config.json, they are overwritten.
 char blynk_token[34] = "3534391eaa0346bda6090415430ceb23";
+const char *host = "esp8266-webupdate"; // Хост обновления
 //flag for saving data
 bool shouldSaveConfig = false;
-//callback notifying us of the need to save config
 
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
+
+//callback notifying us of the need to save config
 void saveConfigCallback()
 {
-    Serial.println("Should save config");
-    shouldSaveConfig = true;
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
 }
 
 void setup()
 {
-    // put your setup code here, to run once:
-    Serial.begin(9600);
-    Serial.println();
-    //clean FS, for testing
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println();
+  // WiFiManager
+    //Очистить FS, для теста
     //SPIFFS.format();
-    //read configuration from FS json
-    Serial.println("Подключение FS...");
+
+    //Считать конфигурацию из FS json
+    Serial.println("Монтирование FS...");
+
     if (SPIFFS.begin())
     {
-        Serial.println("mounted file system");
-        if (SPIFFS.exists("/config.json"))
+      Serial.println("mounted file system");
+      if (SPIFFS.exists("/config.json"))
+      {
+        //file exists, reading and loading
+        Serial.println("reading config file");
+        File configFile = SPIFFS.open("/config.json", "r");
+        if (configFile)
         {
-            //file exists, reading and loading
-            Serial.println("reading config file");
-            File configFile = SPIFFS.open("/config.json", "r");
-            if (configFile)
-            {
-                Serial.println("opened config file");
-                size_t size = configFile.size();
-                // Allocate a buffer to store contents of the file.
-                std::unique_ptr<char[]> buf(new char[size]);
-                configFile.readBytes(buf.get(), size);
-                DynamicJsonBuffer jsonBuffer;
-                JsonObject &json = jsonBuffer.parseObject(buf.get());
-                json.printTo(Serial);
-                if (json.success())
-                {
-                    Serial.println("\nparsed json");
-                    //strcpy(mqtt_server, json["mqtt_server"]);
-                    //strcpy(mqtt_port, json["mqtt_port"]);
-                    strcpy(blynk_token, json["blynk_token"]);
-                }
-                else
-                {
-                    Serial.println("failed to load json config");
-                }
-            }
+          Serial.println("opened config file");
+          size_t size = configFile.size();
+          // Allocate a buffer to store contents of the file.
+          std::unique_ptr<char[]> buf(new char[size]);
+
+          configFile.readBytes(buf.get(), size);
+          DynamicJsonBuffer jsonBuffer;
+          JsonObject &json = jsonBuffer.parseObject(buf.get());
+          json.printTo(Serial);
+          if (json.success())
+          {
+            Serial.println("\nparsed json");
+
+            //strcpy(mqtt_server, json["mqtt_server"]);
+            //strcpy(mqtt_port, json["mqtt_port"]);
+            strcpy(blynk_token, json["blynk_token"]);
+          }
+          else
+          {
+            Serial.println("failed to load json config");
+          }
         }
+      }
     }
     else
     {
-        Serial.println("failed to mount FS");
+      Serial.println("failed to mount FS");
     }
     //end read
 
@@ -107,18 +121,18 @@ void setup()
     //if it does not connect it starts an access point with the specified name
     //here  "AutoConnectAP"
     //and goes into a blocking loop awaiting configuration
-    wifiManager.autoConnect("Shetchik");
+    wifiManager.autoConnect("Schetchik");
     if (!wifiManager.autoConnect("AutoConnectAP", "password"))
     {
-        Serial.println("failed to connect and hit timeout");
-        delay(3000);
-        //reset and try again, or maybe put it to deep sleep
-        ESP.reset();
-        delay(5000);
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.reset();
+      delay(5000);
     }
 
     //if you get here you have connected to the WiFi
-    Serial.println("Подключено...yeey :)");
+    Serial.println("connected...yeey :)");
 
     //read updated parameters
     //strcpy(mqtt_server, custom_mqtt_server.getValue());
@@ -128,33 +142,42 @@ void setup()
     //save the custom parameters to FS
     if (shouldSaveConfig)
     {
-        Serial.println("saving config");
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject &json = jsonBuffer.createObject();
-        //json["mqtt_server"] = mqtt_server;
-        //json["mqtt_port"] = mqtt_port;
-        json["blynk_token"] = blynk_token;
+      Serial.println("saving config");
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject &json = jsonBuffer.createObject();
+      //json["mqtt_server"] = mqtt_server;
+      //json["mqtt_port"] = mqtt_port;
+      json["blynk_token"] = blynk_token;
 
-        File configFile = SPIFFS.open("/config.json", "w");
-        if (!configFile)
-        {
-            Serial.println("failed to open config file for writing");
-        }
+      File configFile = SPIFFS.open("/config.json", "w");
+      if (!configFile)
+      {
+        Serial.println("failed to open config file for writing");
+      }
 
-        json.printTo(Serial);
-        json.printTo(configFile);
-        configFile.close();
-        //end save
+      json.printTo(Serial);
+      json.printTo(configFile);
+      configFile.close();
+      //end save
     }
 
-    Serial.println("Локальный IP");
+    Serial.println("local ip");
     Serial.println(WiFi.localIP());
-    Blynk.config(blynk_token);
-    Blynk.begin(blynk_token, WiFi.SSID().c_str(), WiFi.psk().c_str());
+  // WiFiManager конец
+  Blynk.config(blynk_token);
+  Blynk.begin(blynk_token, WiFi.SSID().c_str(), WiFi.psk().c_str());
+
+  // Обновление
+  MDNS.begin(host);
+  httpUpdater.setup(&httpServer);
+  httpServer.begin();
+  MDNS.addService("http", "tcp", 80);
+  Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your браузере\n", host);
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
-    Blynk.run();
+  Blynk.run();
+  httpServer.handleClient();
+  MDNS.update();
 }
